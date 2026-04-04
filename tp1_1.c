b31d877a9c159907b2193171fceeddb5f33fcb95
martin paez <martinpaez640@gmail.com>
2026-04-03 21:41:03 -0300

    Se completo el ejecicio e del punto 4

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+    int num=50,*Pnum;
+    Pnum=&num;
+    printf("Contenido del puntero %d",*Pnum);
+    printf("\nLa direccion almacenada del puntero %p",Pnum);
+    printf("\nLa direccion de memoria de la variable %p",&num);
+    printf("\nLa direccion de memoria del puntero %p",&Pnum);
+    printf("\nEl tamaño de memoria utilizada en la variable %d",sizeof(Pnum));
     return 0;
 }
